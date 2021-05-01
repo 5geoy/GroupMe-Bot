b90b6cda1c24921a49f26ad1255bbd0b141d109e
5geoy <georgeelia28@gmail.com>
2021-05-01 12:17:26 -0400

    post with a different account

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
-exports.ACCESS_TOKEN = process.env.GM_ACCESS_TOKEN;
-exports.BOT_ID = process.env.BOT_ID;
+exports.ACCESS_TOKEN = process.env.CCYA_ACCESS_TOKEN;
+exports.BOT_ID = process.env.BOT_TESTER_CCYA;
 exports.GROUP_ID = process.env.GROUP_ID;
 
 exports.DEFAULT_MSGS_TO_ANALYZE = "2000";
