68ff86d0d7f1293c7693dbe3d8113bbee17a5d19
5geoy <georgeelia28@gmail.com>
2021-05-01 12:22:58 -0400

    use the real chat!!!

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 exports.ACCESS_TOKEN = process.env.CCYA_ACCESS_TOKEN;
-exports.BOT_ID = process.env.BOT_TESTER_CCYA;
-exports.GROUP_ID = process.env.GROUP_ID;
+exports.BOT_ID = process.env.CCYA_RESIDENTS_BOT_ID;
+exports.GROUP_ID = process.env.CCYA_RESIDENTS_GROUP_ID;
 
 exports.DEFAULT_MSGS_TO_ANALYZE = "2000";
 
